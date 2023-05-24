bb4a33faa5cfa9b181c9c45d5735068210097714
kkyuwoo <kyurikim012@gmail.com>
2023-05-24 13:47:33 +0900

    feat: NUserDao, DUserDao 추가, UserDao 수정 - NUserDao : getConnection abstract 구현 - DUserDao : getConnection abstract 틀 구현 - UserDao : abstract class 수정

--- src/main/java/com/example/tobyspring3/dao/UserDao.java
+++ src/main/java/com/example/tobyspring3/dao/UserDao.java
@@ -7,20 +7,8 @@ import java.util.Map;
 
 import static java.lang.System.getenv;
 
-public class UserDao {
-    //return type이 Connection
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Map<String, String> env = getenv();
-        String dbHost = env.get("DB_HOST");
-        String dbUser = env.get("DB_USER");
-        String dbPassword = env.get("DB_PASSWORD");
-
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conn = DriverManager.getConnection(
-                dbHost, dbUser, dbPassword
-        );
-        return conn;
-    }
+public abstract class UserDao {
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
     public void add(User user) throws ClassNotFoundException, SQLException {
         Connection conn = getConnection();
 
@@ -56,14 +44,14 @@ public class UserDao {
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDao();
+        UserDao userDao = new NUserDao();
         User user = new User();
-        user.setId("3");
+        user.setId("4");
         user.setName("kyeongrok");
         user.setPassword("1234");
         userDao.add(user);
 
-        User selectedUser = userDao.get("3");
+        User selectedUser = userDao.get("4");
         System.out.println(selectedUser.getId());
         System.out.println(selectedUser.getName());
         System.out.println(selectedUser.getPassword());
